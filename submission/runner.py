16c11cd4e0708f2513f721feeae8a68f585c4d6d
Raaghav Raaj <raaghavraaj@Raaghavs-MacBook-Pro.local>
2021-09-05 12:57:18 +0530

    small change

--- submission/runner.py
+++ submission/runner.py
@@ -7,23 +7,23 @@ algos = ['epsilon-greedy-t1', 'ucb-t1', 'kl-ucb-t1',
 horizons = [100, 400, 1600, 6400, 25600, 102400]
 
 # Task 1:
-# for i in range(1, 4):
-#     instance = "../instances/instances-task1/i-{}.txt".format(i)
-#     banditSample = BernoulliBandit(instance)
-#     for algo in algos[0:4]:
-#         for horizon in horizons:
-#             for seed in range(50):
-#                 regret, high = banditSample.runAlgo(
-#                     algo, seed, 0.02, 2, 0, horizon)
+for i in range(1, 4):
+    instance = "../instances/instances-task1/i-{}.txt".format(i)
+    banditSample = BernoulliBandit(instance)
+    for algo in algos[0:4]:
+        for horizon in horizons:
+            for seed in range(50):
+                regret, high = banditSample.runAlgo(
+                    algo, seed, 0.02, 2, 0, horizon)
 
 # Task 2:
-# for i in range(1, 6):
-#     instance = "../instances/instances-task2/i-{}.txt".format(i)
-#     banditSample = BernoulliBandit(instance)
-#     for scale in range(1, 16):
-#         for seed in range(50):
-#             regret, high = banditSample.runAlgo(
-#                 'ucb-t2', seed, 0.02, 0.02*scale, 0, 10000)
+for i in range(1, 6):
+    instance = "../instances/instances-task2/i-{}.txt".format(i)
+    banditSample = BernoulliBandit(instance)
+    for scale in range(1, 16):
+        for seed in range(50):
+            regret, high = banditSample.runAlgo(
+                'ucb-t2', seed, 0.02, 0.02*scale, 0, 10000)
 
 # Task 3:
 for i in range(1, 3):
@@ -35,10 +35,10 @@ for i in range(1, 3):
                 'alg-t3', seed, 0.02, 0.225, 0, horizon)
 
 # Task 4:
-# for i in range(1, 3):
-#     instance = "../instances/instances-task4/i-{}.txt".format(i)
-#     for threshold in [0.2, 0.6]:
-#         for horizon in horizons:
-#             for seed in range(50):
-#                 regret, high = banditSample.runAlgo(
-#                     'alg-t4', seed, 0.02, 2, threshold, horizon)
+for i in range(1, 3):
+    instance = "../instances/instances-task4/i-{}.txt".format(i)
+    for threshold in [0.2, 0.6]:
+        for horizon in horizons:
+            for seed in range(50):
+                regret, high = banditSample.runAlgo(
+                    'alg-t4', seed, 0.02, 2, threshold, horizon)
